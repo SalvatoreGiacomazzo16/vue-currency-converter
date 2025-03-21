f593e3bea077a2832529dfd2f25d23a1a5c26066
SalvatoreGiacomazzo16 <salvatore161giacomazzo@gmail.com>
2025-03-21 18:21:11 +0100

    installed bootstrap and customized component name

--- vue-currency-converter/src/App.vue
+++ vue-currency-converter/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
+import Converter from './components/Converter.vue'
 
 </script>
 
 <template>
-
+<h1 class="text-danger">Currency Converter</h1>
+  
 </template>
 
 <style scoped>
---
+++ vue-currency-converter/src/components/Converter.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+
+
+</script>
+
+<template>
+<h1> Currency Converter</h1>
+</template>
+
+<style scoped>
+
+</style>
