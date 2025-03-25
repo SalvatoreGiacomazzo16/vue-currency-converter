cfee3c45cc12aeab0e488dff895c1538b548ee3a
SalvatoreGiacomazzo16 <salvatore161giacomazzo@gmail.com>
2025-03-25 18:39:02 +0100

    created component for currency input

---
+++ vue-currency-converter/src/components/CurrencyInput.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="currency-input">
+       
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CurrencyInput',
+    props: {
+       
+    },
+    data() {
+        return {
+          
+        };
+    },
+    methods: {
+       
+    },
+    computed: {
+        
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
